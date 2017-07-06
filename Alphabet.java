a9efcf6860420af8dc41dd240c5455a3f02beecb
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-07-06 16:10:37 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,13 @@
+package Guvi;
+
+public class Alphabet {
+	public Alphabet() {
+		for ( int i = 65; i < 91; i ++ ) {
+		System.out.println("Upper Case\n"+(char)i + " " + (char)(i+32));
+		}
+		}
+		public static void main(String [] args) {
+		new Alphabet();
+
+}
+}
