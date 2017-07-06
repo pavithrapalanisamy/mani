fa2e8390f006f499ae19b960a2b92e7e860d4499
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-07-06 16:54:16 +0530

    Add files via upload

---
+++ Keyboard.java
@@ -0,0 +1,19 @@
+package Guvi;
+
+import java.util.Scanner;
+public class Keyboard {
+
+	public static void main(String[] args) {
+
+		Scanner in=new Scanner(System.in);
+		
+		char q;
+		q=in.nextLine().charAt(0);
+		
+		if(q==q)
+		{
+			System.exit(0);
+
+	}
+
+}}
